06b67804a10e8507acbdd50c6a854058dba82988
chaitanyks <defcdr@gmail.com>
2021-03-05 01:19:03 +0530

    change

--- src/main/java/com/demo/practice/controller/UploadController.java
+++ src/main/java/com/demo/practice/controller/UploadController.java
@@ -1,25 +1,12 @@
 package com.demo.practice.controller;
 
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Date;
 
 @RestController
 public class UploadController {
-    @GetMapping(value = "/api/download/{uuid}")
-    public String DownloadFile(MultipartFile multipartFile){
+    @GetMapping(value = "/api/download")
+    public String downloadFile(){
 
-        String str = "This is the new row";
-        return str;
-    }
-
-    @PostMapping(value = "/upload")
-    public void UploadFile(@RequestParam("myFile") MultipartFile file) throws IOException {
-//        Date date = new Date();
-        File temfile = File.createTempFile("temp","date");
-        file.transferTo(temfile);
+        return "This is the new row";
     }
 }
